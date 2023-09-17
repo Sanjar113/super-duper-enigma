abcc1008f2fc154750c388677fcd50bab926bbcb
Hafizbey <hafizfullstack@gmail.com>
2023-09-17 14:22:07 +0600

    no shadow

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, Circle, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import Icon from "../images/icon.png"
 import KaraKoi from "../images/Karakoi.jpeg"
+import { BiSolidCaretRightSquare } from "react-icons/bi"
+
 export const Map = () => {
 
+
+    const [show, setShow] = useState(false)
     const center = [40.878134, 74.975752];
     const circleCenter = [42.84619, 74.58617];
 
@@ -48,8 +52,19 @@ export const Map = () => {
     });
 
     return (
-        <div className=''>
-            <MapContainer center={center} zoom={7} style={{ width: '100%', height: '100vh' }}>
+        <div className='relative'>
+            <div className={`${show} ?  w-[245px] h-full bg-white absolute top-0 left-0 z-10`}>
+                Hello
+            </div>
+            <div className='absolute top-4 left-4 w-max z-10 flex gap-[3px] items-center '>
+                <div className='bg-white flex justify-between items-center rounded-md px-2 py-1 shadow-lg shadow-gray-500/50'>
+                    <input placeholder='Поиск' className='bg-transparent p-1' />
+                </div>
+                <span className=''>
+                    <BiSolidCaretRightSquare className='text-[25px] cursor-pointer text text-[#242424]' onClick={() => setShow(true)} />
+                </span>
+            </div>
+            <MapContainer zoomControl={false} center={center} zoom={7} style={{ width: '100%', height: '100vh' }}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
